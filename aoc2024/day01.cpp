d7e9134d82da6f4e8123ffb780b130335686c875
Jacques Schutte <jqschutte@gmail.com>
2024-12-01 11:22:44 +0200

    Day 01

--- aoc2024/day01.cpp
+++ aoc2024/day01.cpp
@@ -3,8 +3,49 @@ import std;
 #include "day01.hpp"
 #include "log.hpp"
 
-void Day01::input(const std::string_view input) {}
+int convert(std::string_view v) {
+    int output{};
+    std::from_chars(v.data(), v.data() + v.size(), output);
+    return output;
+}
 
-void Day01::part1() const {}
+void Day01::input(const std::string_view input) {
+    for (const auto s : std::ranges::views::split(input, '\n')) {
+        auto line = std::string_view(s);
+        if (!line.empty())
+        {
+            for (auto split : std::ranges::views::split(line, std::string_view("   "))) {
+                if (this->left.size() > this->right.size()) {
+                    this->right.push_back(convert(std::string_view(split)));
+                }
+                else {
+                    this->left.push_back(convert(std::string_view(split)));
+                }
+            }
+        }
+    }
+}
 
-void Day01::part2() const {}
+void Day01::part1() const {
+    auto copy_left = this->left;
+    auto copy_right = this->right;
+
+    std::ranges::sort(copy_left);
+    std::ranges::sort(copy_right);
+
+    auto total = 0;
+    for (const auto [l, r] : std::ranges::views::zip(copy_left, copy_right)) {
+        total += std::abs(l - r);
+    }
+
+    info("Part 1: {}", total);
+}
+
+void Day01::part2() const {
+    auto total = 0;
+    for (const auto l : this->left) {
+        total += l * std::ranges::count(this->right, l);
+    }
+
+    info("Part 2: {}", total);
+}
