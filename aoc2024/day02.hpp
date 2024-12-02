7273b077517b66086681fd4ad3b10da2bf6849b9
Jacques Schutte <jqschutte@gmail.com>
2024-12-02 10:38:08 +0200

    Day 02

--- aoc2024/day02.hpp
+++ aoc2024/day02.hpp
@@ -2,9 +2,15 @@
 
 import std;
 
+struct Report {
+    std::vector<int> values;
+};
+
 struct Day02 {
     static constexpr std::uint8_t number = 2;
     void input(const std::string_view input);
     void part1() const;
     void part2() const;
+
+    std::vector<Report> reports;
 };
