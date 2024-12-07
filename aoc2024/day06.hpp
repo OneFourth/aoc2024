0f87d8f688afa5b26a31770007c16d8ae9d869b8
Jacques Schutte <jqschutte@gmail.com>
2024-12-07 11:35:36 +0200

    Day 06

--- aoc2024/day06.hpp
+++ aoc2024/day06.hpp
@@ -2,9 +2,27 @@
 
 import std;
 
+enum class Direction {
+    Up,
+    Right,
+    Down,
+    Left,
+};
+
+using Point = std::pair<std::size_t, std::size_t>;
+using PointWithDirection = std::tuple<std::size_t, std::size_t, Direction>;
+
 struct Day06 {
     static constexpr std::uint8_t number = 6;
     void input(const std::string_view input);
     void part1() const;
     void part2() const;
+
+    std::string tiles;
+    std::size_t width;
+    std::size_t height;
+
+    auto get_span() const {
+        return std::mdspan(this->tiles.data(), this->width, this->height);
+    };
 };
