5bb687ceddd390bf86fbc12e1dd3aa78f1a2243f
Jacques Schutte <jqschutte@gmail.com>
2024-12-08 14:25:06 +0200

    Day 08

--- aoc2024/day08.hpp
+++ aoc2024/day08.hpp
@@ -2,9 +2,18 @@
 
 import std;
 
+using Pos = std::pair<int, int>;
+
 struct Day08 {
     static constexpr std::uint8_t number = 8;
     void input(const std::string_view input);
     void part1() const;
     void part2() const;
+
+    std::set<Pos> antinodes(const int extrapolation_start, const int extrapolation_count) const;
+    void print_map(const std::set<Pos>& antinodes) const;
+
+    std::size_t width;
+    std::size_t height;
+    std::unordered_map<char, std::vector<Pos>> antenna;
 };
